d3fcfc95f817505a1a5085b60db3e5352c9e8b3f
NataGalvez <nataliac.galvezc@gmail.com>
2019-07-09 07:49:43 -0400

    archivo main separado de data.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-/* Manejo del DOM */
+workData.fillSelect(workData.uniques("name"),"formCharacter");
+workData.fillSelect(workData.uniques("status"),"formStatus");
+workData.fillSelect(workData.uniques("species"),"formSpecies");
+workData.fillSelect(workData.uniques("gender"),"formGender");
+document.getElementById("formCharacter").addEventListener("change", ()=>{
+	workData.cleanDiv();
+
+	let valueSelect=document.getElementById("formCharacter").value;
+	workData.showCard(valueSelect);
+})
+document.getElementById("formStatus").addEventListener("change", ()=>{
+	let valueSelect=document.getElementById("formStatus").value;
+	workData.showCardS(valueSelect,"status");
+})
+document.getElementById("formSpecies").addEventListener("change", ()=>{
+	let valueSelect=document.getElementById("formSpecies").value;
+	workData.showCardS(valueSelect,"species");
+})
+document.getElementById("formGender").addEventListener("change", ()=>{
+	let valueSelect=document.getElementById("formGender").value;
+	workData.showCardS(valueSelect,"gender");
+})
+
