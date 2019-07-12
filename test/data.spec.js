2317b9f4b29022ca219be9f579956df801c70ced
NataGalvez <nataliac.galvezc@gmail.com>
2019-07-12 00:06:28 -0400

    intento test n25.060

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,15 +2,40 @@ global.window = global;
 global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
+require('../src/data/rickandmorty');
+
 
 
 describe('workData', () => {
-  
-  it('debería ser una función', () => {
-    assert.deepequal(typeof example, 'function');
+	const show=[
+	{
+		"name": "Rick Sanchez",
+		"status": "Alive"
+	},
+	{
+		"name": "Retired General Rick",
+		"status": "unknown"
+	},
+	{
+		"name": "Gear Cop",
+   		"status": "Dead"
+	},
+		
+	]
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof workData, 'object');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
-  });
-})
+  describe('workData.showCard', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof workData.showCard, 'function');
+    });
+
+    it('debería debería retornar el objeto Rick Sanchez al filtrar por status Alive', ()=>{
+      assert.deepequal(workData.showCard(show, "Alive"), [{"name": "Rick Sanchez","status": "Alive"}])
+    })
+    
+  })
+  
